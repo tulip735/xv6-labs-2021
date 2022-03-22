bc87b4efb1d295656d6b50494cb71cff46a8cb39
tulip <tulip@gmail.com>
2022-03-23 02:20:03 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,22 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_helper(pagetable_t pagetable, int level) {
+  char *prefix[] = {"..", ".. ..", ".. .. .."};
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    /* if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){ */
+    if((pte & PTE_V)){
+      uint64 child = PTE2PA(pte);
+      printf("%s %d: pte %p pa %p\n", prefix[level], i, pte, child);
+      if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        vmprint_helper((pagetable_t)child, level + 1);
+      }
+    } 
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0);
+}
